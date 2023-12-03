26608038017b16a3f3813635e2174ed473914d0e
bHabawel <rosshabawel12@gmail.com>
2023-12-03 17:40:23 +0800

    using css modules and adding starter folder

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,15 @@
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <>
       <PageNav />
+      <AppNav />
       <div>
-        <h1>WorldWise</h1>
+        <h1 className="test">WorldWise</h1>
+        <Link to="/app">Go to the app</Link>
       </div>
     </>
   );
